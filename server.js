ca6eed7f67a9823df450758e37446d9ca94a669c
SIDDHARTH SHARMA <50225467+is1dd@users.noreply.github.com>
2023-10-25 10:51:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 app.use(cors());
 
-// Connect to MongoDB Atlas
+
 const mongoURI =
   "mongodb+srv://siddharthsharma9660:hellospearmint@cluster0.vfu1dvn.mongodb.net/test";
 const dbConnect = () => {
   return mongoose.connect(mongoURI);
 };
-// Define Stocks Schema
+//Stocks Schema
 const stockSchema = new mongoose.Schema({
   symbol: String,
   price: Number,
@@ -19,7 +19,19 @@ const stockSchema = new mongoose.Schema({
 
 const Stock = mongoose.model("Stock", stockSchema);
 
-// Mock API Endpoint
+const updateStockPrices = async () => {
+  const stocks = await Stock.find();
+  stocks.forEach(async (stock) => {
+    const randomChange = Math.abs(Math.floor(Math.random() * 11) - 5); // Generates a random number between -5 and 5
+    const newPrice = Math.round(stock.price + randomChange);
+    await Stock.updateOne({ _id: stock._id }, { $set: { price: newPrice } });
+  });
+};
+
+// Setting up timer to update stock prices every 10 seconds
+setInterval(updateStockPrices, 1000);
+
+
 
 app.get("/api/stocks/:symbol", async (req, res) => {
   const { symbol } = req.params;
